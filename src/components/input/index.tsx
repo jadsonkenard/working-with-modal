a816d873f31d2a06d61acaa6eb7d286d63333e8a
unknown <jadsonkenard@hotmail.comgit config --global user.email jadsonkenard@hotmail.com>
2024-10-04 16:38:12 -0300

    Criado a opção de ver e ocultar senha pelo icone

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,29 +1,32 @@
-import { TextInput, View, Text } from "react-native";
+import { TextInput, View, Text, TouchableOpacity, TextInputProps } from "react-native";
 import { styles } from "./styles";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { theme } from "../../theme";
 
-type InputProps = {
+type InputProps = TextInputProps & {
   title: string;
   icon: keyof typeof FontAwesome.glyphMap;
   eye?: keyof typeof FontAwesome.glyphMap;
   error?: boolean;
+  onPrees?: () => void;
 };
 
-export function Input({ title, icon, eye, error }: InputProps) {
-    const borderColor = error ? theme.colors.danger : theme.colors.primary;
+export function Input({ title, icon, eye, error, onPrees, ...props }: InputProps) {
+  const borderColor = error ? theme.colors.danger : theme.colors.primary;
   return (
     <View style={{ marginTop: 12 }}>
       <View style={styles.viewTitle}>
-        <Text style={[styles.title, {color: borderColor}]}>{title}</Text>
+        <Text style={[styles.title, { color: borderColor }]}>{title}</Text>
       </View>
-      <View style={[styles.container, {borderColor}]}>
+      <View style={[styles.container, { borderColor }]}>
         <View style={styles.icon}>
           <FontAwesome name={icon} size={21} color={borderColor} />
         </View>
-        <TextInput style={styles.content} />
+        <TextInput style={styles.content} {...props}/>
         <View style={styles.eye}>
-          <FontAwesome name={eye} size={21} color={borderColor} />
+          <TouchableOpacity onPress={onPrees}>
+            <FontAwesome name={eye} size={21} color={borderColor} />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { View } from "react-native";
 import { styles } from "./styles";
 import { Input } from "../../components";
 
 export function Login() {
+  const [error, setError] = useState(true);
+  const [hidePass, setHidePass] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Input title="Email" icon="envelope-o" error />
-      <Input title="Senha" icon="lock" eye="eye" />
+      <Input title="Email" icon="envelope-o" error={error} />
+      <Input
+        title="Senha"
+        icon="lock"
+        eye={hidePass ? "eye" : "eye-slash"}
+        secureTextEntry={hidePass}
+        onPrees={() => setHidePass(!hidePass)}
+        error
+      />
     </View>
   );
 }
